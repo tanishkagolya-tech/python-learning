8840e61ebf2a39e8fba0f9f8409008213a137900
Tanishka Golya <tanishkagolya224@gmail.com>
2026-01-02 21:06:23 +0530

    day 3: multiple if else examples

---
+++ day3_if_else.py
@@ -0,0 +1,49 @@
+#Example 1
+age = 18
+if age >= 18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+#Example 2 - Number check
+number = 5
+if number > 0:
+    print("Positive number")
+else:
+    print("Zero or Negative number")
+
+# Example 3 - Even or Odd number
+
+num = 8
+if num % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+
+# Example 4 - Pass or Fail
+marks = 72
+
+if marks >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+# Example 5 - Largest of two numbers
+
+a = 10
+b = 25
+if a > b:
+    print("a is greater")
+else:
+    print("b is greater")
+
+# Example 6 - Temperature check
+
+temperature = 30
+if temperature > 25:
+    print("Hot weather")
+else:
+    print("Cool weather")
+
+
+
